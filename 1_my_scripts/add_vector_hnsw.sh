53664ceb54e14565ca38167b2f96f766f2762a55
Haijun Yu <yuhj@zetyun.com>
2023-07-14 18:33:07 +0800

    update ad 2048 -> 1000

--- 1_my_scripts/add_vector_hnsw.sh
+++ 1_my_scripts/add_vector_hnsw.sh
@@ -2,5 +2,5 @@
 
 
 cd ../build/bin/
-./dingodb_client_store  --method=VectorAddBatch --region_id=80003 --dimension=1024 --count=1000000  --step_count=2048  --vector_enable_scalar=false --start_id=0 --timeout_ms=1000000 --log_each_request=false --vector_index_add_cost_file="./hnsw-1000000.txt"
+./dingodb_client_store  --method=VectorAddBatch --region_id=80003 --dimension=1024 --count=1000000  --step_count=1000  --vector_enable_scalar=false --start_id=0 --timeout_ms=1000000 --log_each_request=false --vector_index_add_cost_file="./hnsw-1000000.txt"
 
