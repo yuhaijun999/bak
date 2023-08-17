4a93b496e871a25c07fbf7e0b36744bcf4c6fbb2
Haijun Yu <yuhj@zetyun.com>
2023-08-17 16:42:41 +0800

    add vector index search for sql

---
+++ vector_index_search/search_vector_hnsw_debug_top100.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+cd ../build/bin/
+./dingodb_client  --method=VectorSearchDebug --region_id=80003 --dimension=1024 --topn=100 --vector_id=1 --batch_count=1 --vector_ids_count=100 --without_vector=true --with_scalar=false --with_table=false    --timeout_ms=1000000 --print_vector_search_delay=true
+
