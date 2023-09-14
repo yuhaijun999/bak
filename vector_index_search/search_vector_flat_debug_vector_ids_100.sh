6dc9ebb1d6e4db1448b3748969518474b131f0b5
Haijun Yu <yuhj@zetyun.com>
2023-09-14 18:03:48 +0800

    update vector index search

--- vector_index_search/search_vector_flat_debug_vector_ids_100.sh
+++ vector_index_search/search_vector_flat_debug_vector_ids_100.sh
@@ -2,5 +2,5 @@
 
 
 cd ../build/bin/
-./dingodb_client  --method=VectorSearchDebug --region_id=80001 --dimension=1024 --topn=10 --vector_id=0 --batch_count=1 --vector_ids_count=100 --without_scalar=true --without_table=true --with_table=false  --with_vector_ids=true   --timeout_ms=1000000 --print_vector_search_delay=true
+./dingodb_client  --method=VectorSearchDebug --region_id=80001 --dimension=1024 --topn=10 --vector_id=0 --batch_count=1 --vector_ids_count=100 --without_scalar=true --without_table=true  --with_vector_ids=true   --timeout_ms=1000000 --print_vector_search_delay=true
 
