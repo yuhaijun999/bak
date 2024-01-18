8bfca8d025cf0952b93832aa33bd74af67862be8
Haijun Yu <yuhj@zetyun.com>
2024-01-18 17:08:41 +0800

    add vector index table data

---
+++ vector_index_table_data/search_vector_flat_with_table_pre_filter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+cd ../build/bin/
+./dingodb_client  --method=VectorSearch --region_id=80001 --dimension=8 --topn=10 --vector_id=0  --without_vector=false --without_scalar=false --without_table=false  --with_table_pre_filter=true  --timeout_ms=1000000
+
