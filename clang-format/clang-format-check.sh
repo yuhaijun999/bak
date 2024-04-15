6e72831180848c66f9f5c2b7cad859cd15393a46
Haijun Yu <yuhj@zetyun.com>
2024-04-15 10:54:32 +0800

    update clang-format-check

--- clang-format/clang-format-check.sh
+++ clang-format/clang-format-check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eo pipefail
-git ls-files | grep -E  '\.(cpp|h|cu|cuh)$' | xargs clang-format -i
+git ls-files | grep -E  '\.(cpp|h|cu|cuh|cxx|hpp|hxx)$' | xargs clang-format -i
 if git diff --quiet; then
   echo "Formatting OK!"
 else
