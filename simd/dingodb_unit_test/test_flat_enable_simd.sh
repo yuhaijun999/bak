608f524689100f650e01eec85b5abbecc9a5c3b7
Haijun Yu <yuhj@zetyun.com>
2024-05-15 16:16:26 +0800

    add simd script

---
+++ simd/dingodb_unit_test/test_flat_enable_simd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+./dingodb_unit_test  --gtest_filter=VectorIndexFlatSmidTest.*  --vector_index_flat_smid_test_dimension=512 --vector_index_flat_smid_test_data_base_size=10000000 --vector_index_flat_smid_test_enable_simd=true
+
