9438888282f6401d53fbaf6af6232439cba81ab7
Haijun Yu <yuhj@zetyun.com>
2024-05-15 18:27:35 +0800

    update simd script

--- simd/dingodb_unit_test/test_hnsw_disable_simd.sh
+++ simd/dingodb_unit_test/test_hnsw_disable_simd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./dingodb_unit_test  --gtest_filter=VectorIndexHnswSmidTest.*  --vector_index_hnsw_smid_test_dimension=512  --vector_index_hnsw_smid_test_data_base_size=10000000  --vector_index_hnsw_smid_test_enable_simd=false
+./dingodb_unit_test  --gtest_filter=VectorIndexHnswSimdTest.*  --vector_index_hnsw_smid_test_dimension=512  --vector_index_hnsw_smid_test_data_base_size=10000000  --vector_index_hnsw_smid_test_enable_simd=false
 
