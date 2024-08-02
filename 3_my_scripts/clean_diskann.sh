fb01a5647a680eae30966d9079dac921378c9fc4
Haijun Yu <yuhj@zetyun.com>
2024-08-02 11:01:05 +0800

    add 3 copy diskann

---
+++ 3_my_scripts/clean_diskann.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+function clean_store(){
+    local role_dir=$1
+    echo "role: ${role_dir}"
+
+    if [ -d ${role_dir}/data ]; then
+        rm ${role_dir}/data/* -rf
+    else
+        mkdir -p ${role_dir}/data
+    fi
+
+
+    if [ -d ${role_dir}/log ] ; then
+        rm ${role_dir}/log/* -rf
+    else
+        mkdir -p ${role_dir}/log
+    fi
+
+
+    tree ${role_dir}
+
+}
+
+
+clean_store diskann1
