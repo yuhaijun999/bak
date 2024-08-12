ccc5cfbc61fc2efcd641f2bdc10c6054f9a821eb
Haijun Yu <yuhj@zetyun.com>
2024-08-12 06:00:17 +0000

    update diskann script

--- my_scripts/start_diskann.sh
+++ my_scripts/start_diskann.sh
@@ -4,9 +4,10 @@ export TCMALLOC_SAMPLE_PARAMETER=524288
 echo "export TCMALLOC_SAMPLE_PARAMETER=524288, to enable heap profiler"
 
 ulimit -n 40960
-ulimit -u 40960
+#ulimit -u 40960
 
 cd diskann1
-./bin/dingodb_server -role=diskann &
+mkdir -p ./log
+./bin/dingodb_server -role=diskann  > ./log/out 2>&1 &
 cd ..
 
